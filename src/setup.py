1aebd03a6948c831b72ddfa49e8ddbdc7507e5f1
sdunning <sdunning@mtk-cms-sdunni.digi.com>
2010-12-15 10:52:50 -0800

    oi

---
+++ src/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import py2exe
+import os
+from sys import path
+
+path.insert(0, "src")
+python_modules = []
+dirs = ["src", "../../src", "../../utils"]
+for dir in dirs:
+    for filename in os.listdir(dir):
+        try:
+            extension = filename[-3:]
+            core_filename = filename[:-3]
+            if extension == ".py" and core_filename not in python_modules:
+                python_modules.append(core_filename)
+        except:
+            pass
+    
+setup(console=['main_sim.py'], 
+      options={
+               "py2exe": {
+                         "includes": python_modules,
+                         "packages": ["cwm_data"]
+                         }
+               }
+      )
